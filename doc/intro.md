ffd8e32adf76e22d3c47b6a292780090b1aec9b3
Thiago Fernandes <thiago@internetbudi.com.br>
2015-10-20 17:34:43 -0200

    initial lein

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to aws-teste
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/aws_teste/core_test.clj
@@ -0,0 +1,7 @@
+(ns aws-teste.core-test
+  (:require [clojure.test :refer :all]
+            [aws-teste.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
